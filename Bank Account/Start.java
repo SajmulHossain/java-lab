47bf31217792203eb8d93a33e545db2bdf19b688
Sajmul <sajmul1427@gmail.com>
2025-11-22 21:42:07 +0600

    bank account --> formatted

--- Bank Account/Start.java
+++ Bank Account/Start.java
@@ -3,12 +3,10 @@ class Account {
     private String accountHolderName;
     private double balance;
 
-    // Constructor
     public Account() {
         System.out.println("Account Created.");
     }
 
-    // Setters
     public void setAccountNumber(String accountNumber) {
         this.accountNumber = accountNumber;
     }
@@ -21,7 +19,6 @@ class Account {
         this.balance = balance;
     }
 
-    // Getters
     public String getAccountNumber() {
         return accountNumber;
     }
@@ -34,7 +31,6 @@ class Account {
         return balance;
     }
 
-    // Transaction Methods
     public void depositMoney(double amount) {
         if (amount > 0) {
             this.balance += amount;
@@ -54,12 +50,10 @@ class Account {
     }
 }
 
-// 2. Child Class: FixedAccount (Extends Account)
 class FixedAccount extends Account {
     private int tenureYear;
 
     public FixedAccount() {
-        // Implicitly calls super()
         System.out.println("Fixed Account initialized.");
     }
 
@@ -72,12 +66,10 @@ class FixedAccount extends Account {
     }
 }
 
-// 3. Child Class: SavingsAccount (Extends Account)
 class SavingsAccount extends Account {
     private double interestRate;
 
     public SavingsAccount() {
-        // Implicitly calls super()
         System.out.println("Savings Account initialized.");
     }
 
@@ -90,50 +82,38 @@ class SavingsAccount extends Account {
     }
 }
 
-// 4. Main Class: Start
 public class Start {
     public static void main(String[] args) {
-
-        // --- Demonstrating FixedAccount ---
         System.out.println("=== Fixed Account Demonstration ===");
         FixedAccount fa = new FixedAccount();
 
-        // Setting inherited attributes (from Account)
         fa.setAccountNumber("FA-101");
         fa.setAccountHolderName("John Doe");
         fa.setBalance(5000.0);
-
-        // Setting specific attributes (from FixedAccount)
         fa.setTenureYear(5);
 
-        // Demonstrating methods
         System.out.println("Holder: " + fa.getAccountHolderName());
         System.out.println("Tenure Years: " + fa.getTenureYear());
         System.out.println("Initial Balance: " + fa.getBalance());
 
-        fa.depositMoney(1000); // Inherited method
-        fa.withdrawMoney(500); // Inherited method
+        fa.depositMoney(1000);
+        fa.withdrawMoney(500);
         System.out.println("Current Balance: " + fa.getBalance());
         System.out.println();
 
-        // --- Demonstrating SavingsAccount ---
         System.out.println("=== Savings Account Demonstration ===");
         SavingsAccount sa = new SavingsAccount();
 
-        // Setting inherited attributes (from Account)
         sa.setAccountNumber("SA-202");
         sa.setAccountHolderName("Jane Smith");
         sa.setBalance(2000.0);
-
-        // Setting specific attributes (from SavingsAccount)
         sa.setInterestRate(3.5);
 
-        // Demonstrating methods
         System.out.println("Holder: " + sa.getAccountHolderName());
         System.out.println("Interest Rate: " + sa.getInterestRate() + "%");
         System.out.println("Initial Balance: " + sa.getBalance());
 
-        sa.withdrawMoney(3000); // Trying to withdraw more than balance
+        sa.withdrawMoney(3000);
         sa.depositMoney(500);
         System.out.println("Final Balance: " + sa.getBalance());
     }
---
+++ Car/Main.java
@@ -0,0 +1,41 @@
+package Car;
+
+class Car {
+    private String brand;
+    private int top_speed;
+    private float milage;
+    private String model;
+
+    public Car(String brand, int top_speed, float milage, String model) {
+        this.brand = brand;
+        this.top_speed = top_speed;
+        this.milage = milage;
+        this.model = model;
+    }
+
+    public float getMilage() {
+        return milage;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getTop_speed() {
+        return top_speed;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Car car1 = new Car("Toyota", 100, 20, "Allion");
+        System.out.println("Brand name is: " + car1.getBrand());
+        System.out.println("Model name is: " + car1.getModel());
+        System.out.println("Milage name is: " + car1.getMilage());
+        System.out.println("Car Top Speed: " + car1.getTop_speed());
+    }
+}
